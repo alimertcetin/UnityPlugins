5f7c47f3c574ca76b6b8f4f61278a667f404bbe9
alimertcetin <mrtmert54@gmail.com>
2023-04-26 11:58:50 +0300

    UpgradeSystem update
    UpgradeSO - FixName moved to Editor script

--- UnityPlugins/Assets/XIV/UpgradeSystem/Integration/UpgradeSO.cs
+++ UnityPlugins/Assets/XIV/UpgradeSystem/Integration/UpgradeSO.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 namespace XIV.UpgradeSystem.Integration
 {
-    public abstract class UpgradeSO<T> : ScriptableObject, IUpgrade<T> where T : Enum
+    public abstract class UpgradeSO : ScriptableObject
     {
-        [SerializeField] int level;
-        [SerializeField] float power;
-        [SerializeField] T type;
-        public int upgradeLevel => level;
-        public T upgradeType => type;
-        public float upgradePower => power;
-        
-        public abstract bool IsBetterThan(IUpgrade<T> other);
-        public abstract bool Equals(IUpgrade<T> other);
-        
 #if UNITY_EDITOR
         /// <summary>
         /// Make sure overriden method is editor only
@@ -26,27 +16,19 @@ namespace XIV.UpgradeSystem.Integration
         //     name = this.title + "_UpgradeDB";
         //     instanceID = this.GetInstanceID();
         // }
-
-        public virtual void FixName()
-        {
-            GetName(out string name, out int instanceID);
-            
-            string assetPath = AssetDatabase.GetAssetPath(instanceID);
-            var splitedPath = assetPath.Split('/');
-            var nameOfAsset = splitedPath[splitedPath.Length - 1];
-            if (name != nameOfAsset)
-            {
-                var error = AssetDatabase.RenameAsset(assetPath, name);
-                if (string.IsNullOrEmpty(error) == false)
-                {
-                    Debug.LogError("Couldnt rename the asset. " + error);
-                }
-                else
-                {
-                    AssetDatabase.SaveAssets();
-                }
-            }
-        }
 #endif
+    }
+    
+    public abstract class UpgradeSO<T> : UpgradeSO, IUpgrade<T> where T : Enum
+    {
+        [SerializeField] int level;
+        [SerializeField] float power;
+        [SerializeField] T type;
+        public int upgradeLevel => level;
+        public T upgradeType => type;
+        public float upgradePower => power;
+        
+        public abstract bool IsBetterThan(IUpgrade<T> other);
+        public abstract bool Equals(IUpgrade<T> other);
     }
 }
